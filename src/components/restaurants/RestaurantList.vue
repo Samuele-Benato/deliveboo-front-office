46c4367bbc62f0958a02f8ffe977320cd28bfacf
Samuele-Benato <samuele.benato@gmail.com>
2023-11-27 20:34:31 +0100

    feat : restaurantList

---
+++ src/components/restaurants/RestaurantList.vue
@@ -0,0 +1,21 @@
+<script>
+import RestaurantCard from './RestaurantCard.vue';
+export default {
+    components: {
+    RestaurantCard,
+  },
+
+  props:{
+      restaurants: Object,
+    },
+};
+</script>
+
+<template>
+    <RestaurantCard class="my-3" v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant" />
+    <h1>list</h1>
+</template>
+
+<style lang="scss">
+ 
+</style>
