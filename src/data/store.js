e52f211e784330c90d8e013f4f107f0a67bc3b02
Samuele-Benato <samuele.benato@gmail.com>
2023-11-27 20:34:57 +0100

    feat : add store.js

---
+++ src/data/store.js
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  api: {
+    baseUrl: 'http://127.0.0.1:8000/api/',
+
+    //Uso "http://localhost:8000/api/" per vedere le img
+    // Altrimenti uso http://127.0.0.1:8000/api/
+  },
+});
