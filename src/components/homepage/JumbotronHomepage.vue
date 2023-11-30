91dbf9c5e72464c8bb289d8df6f602327cbd4b2b
Samuele-Benato <samuele.benato@gmail.com>
2023-12-01 00:53:37 +0100

    fix : jumbotron

--- src/components/homepage/JumbotronHomepage.vue
+++ src/components/homepage/JumbotronHomepage.vue
@@ -4,17 +4,28 @@
 
 <template>
     <div class="jumbotron">
-       <div class="title-jumbo">
-        <h1 class="text-center fw-bold">
-           Di tutto e di più ! <br>
-           Cosa aspetti ? <br>
-           Ordina da Deliveboo !
-        </h1>
-       </div>
+        <div class="banner">
+            <div class="title-jumbo">
+                <h1 class="text-center fw-bold">
+                Di tutto e di più ! <br>
+                Cosa aspetti ? <br>
+                Ordina da Deliveboo !
+                </h1>
+            </div>
+        </div>     
     </div>
 </template>
 
 <style lang="scss" scoped>
+.banner{
+    background-color: rgba($color: #000, $alpha: 0.5) !important;
+    min-height: 50vh;
+    height: 100%;
+    margin-top: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 .jumbotron{
     background-image: url("img/jumbotron.jpg");
     background-size: cover;
@@ -23,13 +34,10 @@
     height: 100%;
     margin-top: 80px;
      
-    display: flex;
-    align-items: center;
-    justify-content: center;
 }
 
 .title-jumbo{
-    background-color: rgba($color: #000, $alpha: 0.8);
+    background-color: rgba($color: #000, $alpha: 0.6);
     padding: 0.5rem;
     border-radius: 5px;
     width: 100%;
