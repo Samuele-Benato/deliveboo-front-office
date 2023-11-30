54534cf6dafceed6c6cd1778a5a17d78c60ab1c1
filippo199600 <pippotartaglia@gmail.com>
2023-11-30 16:00:49 +0100

    fix api menu

--- src/components/plates/PlateList.vue
+++ src/components/plates/PlateList.vue
@@ -23,61 +23,47 @@ export default {
             this.$route.params.restaurantId
         )
         .then((response) => {
-          console.log(response); // Aggiungi questa riga per vedere cosa restituisce la chiamata API
+          console.log(response.data); // Aggiungi questa riga per vedere cosa restituisce la chiamata API
           this.plates = response.data.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching plates:", error);
         });
     },
+  },
 
-    // fetchTypes() {
-    //   axios.get(store.api.baseUrl + "types").then((response) => {
-    //     this.types = response.data.map((type) => {
-    //       return {
-    //         ...type,
-    //         active: false,
-    //       };
-    //     });
-    //   });
-    // },
-
-    // toggleType(type) {
-    //   type.active = !type.active;
-    //   this.fetchRestaurants();
-    // },
-
-    // computed: {
-    // //   activeFilters() {
-    // //     const activeTypes = [];
-
-    // //     this.types.forEach((type) => {
-    // //       if (type.active) activeTypes.push(type.id);
-    // //     });
-
-    // //     return {
-    // //       activeTypes,
-    // //     };
-    // //   },
-    // },
-
-    created() {
-      //   this.fetchTypes();
-      this.fetchPlates();
-      // this.fetchRestaurantList();
-    },
+  created() {
+    this.fetchPlates();
   },
 };
 </script>
 
 <template>
   <div class="wrapper">
-    <div v-for="plate in plates" :key="plate.id">
-      <h1>{{ plate.name }}</h1>
+    <div class="container">
+      <h1>Piatti</h1>
+      <div class="row">
+        <div v-for="plate in plates" :key="plate.id" class="col-md-4 my-2">
+          <div class="card">
+            <img :src="plate.image" class="card-img-top" alt="Plate Image" />
+            <div class="card-body">
+              <h5 class="card-title">{{ plate.name }}</h5>
+              <p class="card-text">{{ plate.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <h1>Piatti</h1>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper {
-  padding-top: 200px;
+  padding-top: 100px;
+}
+
+.card {
+  height: 100%;
+  margin: 10px;
 }
 </style>
