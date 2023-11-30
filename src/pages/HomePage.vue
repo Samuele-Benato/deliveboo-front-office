50fbc198e79a037ec19eeb9010257f63a62913e6
Samuele-Benato <samuele.benato@gmail.com>
2023-11-30 18:21:27 +0100

    fix : homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,13 +2,12 @@
 import JumbotronHomepage from "../components/homepage/JumbotronHomepage.vue";
 import RestaurantsTypeSection from "../components/homepage/RestaurantsTypeSection.vue";
 // import FilterType from "../components/homepage/FilterType.vue";
-import MenuSection from "../components/menu/MenuSection.vue";
+
 
 export default {
   components: {
     JumbotronHomepage,
     RestaurantsTypeSection,
-    MenuSection,
     // FilterType,
   },
 };
@@ -18,7 +17,6 @@ export default {
   <JumbotronHomepage />
   <!-- <FilterType /> -->
   <RestaurantsTypeSection />
-  <MenuSection />
 </template>
 
 <style lang="scss" scoped></style>
