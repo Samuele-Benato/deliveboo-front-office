cf7c19cbc6081e114eef9a2d9f1dad995afc3de2
Samuele-Benato <samuele.benato@gmail.com>
2023-11-30 18:21:51 +0100

    feat : add RestaurantMenu

---
+++ src/pages/RestaurantMenu.vue
@@ -0,0 +1,15 @@
+<script>
+import MenuSection from "../components/menu/MenuSection.vue";
+
+export default {
+  components: {
+    MenuSection,
+  },
+};
+</script>
+
+<template>
+  <MenuSection />
+</template>
+
+<style lang="scss" scoped></style>
