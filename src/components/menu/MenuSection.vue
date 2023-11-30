a71fc79baf1a4da5f381af8ff5cc2f41a283a51a
Samuele-Benato <samuele.benato@gmail.com>
2023-11-30 18:20:08 +0100

    feat : add api call to MenuSection

--- src/components/menu/MenuSection.vue
+++ src/components/menu/MenuSection.vue
@@ -7,11 +7,33 @@ import PlateCard from "./PlateCard.vue";
 export default {
   data() {
     return {
-      
+      plates:[],
     };
   },
 
-  components: {  PlateCard }
+  components: {  PlateCard },
+
+  methods: {
+    fetchPlates() {
+      axios
+        .get(
+          store.api.baseUrl +
+            "plates-by-restaurant/" +
+            this.$route.params.restaurantId
+        )
+        .then((response) => {
+          console.log(response.data); // Aggiungi questa riga per vedere cosa restituisce la chiamata API
+          this.plates = response.data.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching plates:", error);
+        });
+    },
+  },
+
+  created() {
+    this.fetchPlates();
+  },
 }
 </script>
 
@@ -21,7 +43,7 @@ export default {
         <div class="container">
             <h2 class="title-menu">Il nostro menù</h2>
             <div class="row">
-                <PlateCard />   
+              <PlateCard  v-for="plate in plates" :key="plate.id" :plate="plate"/>  
             </div>
         </div>  
         </div>
@@ -35,6 +57,7 @@ export default {
   background-image: url("img/menu-background.jpg");
   background-size: 100%;
   background-position: start;
+  background-attachment: fixed;
   min-height: 50vh;
   height: 100%;
 }
@@ -42,6 +65,8 @@ export default {
   background-color: rgba($color: #000, $alpha: 0.8);
   padding: 0.5rem;
   width:100%;
+  min-height: 100vh;
+  height: 100%;
   padding-top: 5rem;
 }
 
@@ -50,5 +75,6 @@ export default {
   color: #d7d3d3;
   font-weight: 700;
   font-size: 5rem;
+  margin-top: 2rem;
 }
 </style>
