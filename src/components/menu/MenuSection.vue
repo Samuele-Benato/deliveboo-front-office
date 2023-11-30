ceff28a6d54c6e0165f1dc91b3d671327fa3ee4d
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-11-30 20:06:26 +0100

    feat detail restaurant in menu

--- src/components/menu/MenuSection.vue
+++ src/components/menu/MenuSection.vue
@@ -3,17 +3,23 @@ import axios from "axios";
 import { store } from "../../data/store";
 import PlateCard from "./PlateCard.vue";
 
-
 export default {
   data() {
     return {
-      plates:[],
+      plates: [],
     };
   },
 
-  components: {  PlateCard },
+  components: { PlateCard },
 
   methods: {
+    detailRestaurant(plate, index) {
+      return (
+        index === 0 ||
+        plate.restaurant.id !== this.plates[index - 1].restaurant.id
+      );
+    },
+
     fetchPlates() {
       axios
         .get(
@@ -34,25 +40,36 @@ export default {
   created() {
     this.fetchPlates();
   },
-}
+};
 </script>
 
 <template>
-    <div class="restaurants-menu">
-        <div class="container-menu">
-        <div class="container">
-            <h2 class="title-menu">Il nostro menù</h2>
-            <div class="row">
-              <PlateCard  v-for="plate in plates" :key="plate.id" :plate="plate"/>  
-            </div>
-        </div>  
+  <div class="restaurants-menu">
+    <div class="container-menu">
+      <div class="container">
+        <h2 class="title-menu">Il nostro ristorante</h2>
+        <div
+          class="text-center my-3"
+          v-for="(plate, index) in plates"
+          :key="index"
+        >
+          <h3 v-if="detailRestaurant(plate, index)">
+            <h4>{{ plate.restaurant.name }}</h4>
+            <h4>{{ plate.restaurant.address }}</h4>
+            <h4>{{ plate.restaurant.description }}</h4>
+            <h4>{{ plate.restaurant.phone }}</h4>
+          </h3>
         </div>
+        <h2 class="title-menu">Il nostro menù</h2>
+        <div class="row">
+          <PlateCard v-for="plate in plates" :key="plate.id" :plate="plate" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
-
 .restaurants-menu {
   background-image: url("img/menu-background.jpg");
   background-size: 100%;
@@ -64,7 +81,7 @@ export default {
 .container-menu {
   background-color: rgba($color: #000, $alpha: 0.8);
   padding: 0.5rem;
-  width:100%;
+  width: 100%;
   min-height: 100vh;
   height: 100%;
   padding-top: 5rem;
