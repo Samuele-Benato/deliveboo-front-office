d9ba035976972a890d15102b0ecd9114db14c66f
Samuele-Benato <samuele.benato@gmail.com>
2023-12-01 00:09:37 +0100

    fix : style PlateCard

--- src/components/menu/PlateCard.vue
+++ src/components/menu/PlateCard.vue
@@ -41,17 +41,18 @@ export default {
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bree+Serif&family=EB+Garamond:ital,wght@0,500;1,800&display=swap');
 .plate-container{
-    box-shadow: 0 1px 20px 1px grey;
-	background: rgba(#333, 0.90);
-    border-radius: 5px;
-    width: 100%;
+   width: 100%;
 }
 .details-container {
+    box-shadow: 0 1px 20px 1px grey;
+	background: rgba(#333, 0.90);
+    border-radius:10px;
     text-align: center;
     overflow: hidden;
     margin: 2em auto;
     height: 350px;
     position: relative;
+  
     .plate-details {
       position: relative;
       text-align: left;
@@ -60,7 +61,7 @@ export default {
       float: left;
 
       &::-webkit-scrollbar {
-        width: 8px;
+        width: 5px;
       }
 
       &::-webkit-scrollbar-track {
@@ -103,16 +104,16 @@ export default {
 .btn {
 	transform: translateY(0px);
 	transition: 0.3s linear;
-	background:  rgb(50, 140, 54);
+	background:  rgb(20, 91, 23);
 	border-radius: 5px;
   position: relative;
   overflow: hidden;
 	cursor: pointer;
 	outline: none;
 	border: none;
-	color: #eee;
+	color: #111;
 	padding: 0;
-	margin: 0;	
+	margin: 10px 0;	
 }
 .btn:hover{
     transform: translateY(-6px);
@@ -127,7 +128,7 @@ export default {
 	margin:0;	
 }
 .btn .price, .shopping-cart{
-	background: #333;
+	background: #999;
 	border: 0;
 	margin: 0;
 }
@@ -137,7 +138,7 @@ export default {
 .btn .shopping-cart {
 	transform: translateX(-100%);
     position: absolute;
-	background: #555;
+	background: #999;
 	z-index: 1;
     left: 0;
     top: 0;
@@ -158,7 +159,7 @@ export default {
 }
 
 .image-container img {
-    width: 100%;
+    width: 250px;
     aspect-ratio: 1;
     border-radius: 5px;
 }
@@ -166,6 +167,7 @@ export default {
 .info {
     background: rgba(196, 192, 192, 0.9);
     font-family: 'Bree Serif', serif;
+    border-radius: 10px;
     transition: all 0.3s ease-out;
     transform: translateX(-100%);
     position: absolute;
@@ -174,13 +176,12 @@ export default {
     font-size: 105%;
     cursor: no-drop;
     color: #333;
-    height: 100%;
-    width: 100%;
+   padding: 1.5rem;
     left: 0;
     top: 0;
 }
 
-.info h2 {text-align: center;padding-top: 0.5rem;}
+.info h2 {text-align: center;}
 .plate-image:hover .info{transform: translateX(0);}
 .info ul li{transition: 0.3s ease;}
 .info ul li:hover{transform: translateX(50px) scale(1.3);}
@@ -189,7 +190,7 @@ export default {
 
 @media only screen and (min-width: 1340px) {
     .plate-container {
-        width: 32%;
+        width:48%;
     }
 }
 
@@ -200,11 +201,18 @@ export default {
     }
 }
 
+@media only screen and (max-width: 1200px) and (min-width: 993px) {
+    .image-container img {
+    width: 200px;
+}
+}
+
 /* Schermi con larghezza inferiore a 993px */
 @media only screen and (max-width: 992px) {
     .plate-container {
         width: 100%;
     }
+    
 }
 
 @media (max-width: 768px) {
