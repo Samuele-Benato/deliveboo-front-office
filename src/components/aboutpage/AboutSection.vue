e4a34b65f80b854ebe63d962d7f99e5d7cd1f0ee
Samuele-Benato <samuele.benato@gmail.com>
2023-12-04 12:03:09 +0100

    feat : add AboutSection

---
+++ src/components/aboutpage/AboutSection.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<style scoped lang="scss">
+.about-background {
+  background-image: url("img/menu-background.jpg");
+  background-size: 100%;
+  background-size: cover;
+  background-position: start;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  min-height: 50vh;
+  height: 100%;
+}
+.about-container {
+  background-color: rgba($color: #000, $alpha: 0.8);
+  width: 100%;
+  min-width: 540px;
+  min-height: 100vh;
+  height: 100%;
+  padding-top: 1rem;
+  padding-left: 3rem;
+}
+</style>
