bf054a5139b7ce4cad9f271d6bcf9041046d7ef6
Alessandroang <alessandroang94@gmail.com>
2024-01-20 16:24:49 +0100

    Add image

--- src/components/aboutpage/AboutSection.vue
+++ src/components/aboutpage/AboutSection.vue
@@ -1,85 +1,132 @@
 <template>
-<div class="about-background">
+  <div class="about-background">
     <div class="about-container">
-        <div class="mobile">
-            <span class="about-title">Team 5 classe #103</span>
+      <div class="mobile">
+        <span class="about-title">Team 5 classe #103</span>
+      </div>
+      <div class="row one">
+        <div class="card">
+          <img
+            class="samuele-white"
+            src="/img/team/samuele-white.jpg"
+            alt="Samuele Benato"
+          />
+          <img
+            class="samuele-black"
+            src="/img/team/samuele.png"
+            alt="Samuele Benato"
+          />
+          <div class="info">
+            <h2>Samuele Benato</h2>
+            <p>
+              Mi definisco un ragazzo caparbio ed intraprendente, motivarmi e
+              motivare è il mio punto di forza.
+            </p>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/samuele-benato-a9615a29b/"
+                ><font-awesome-icon :icon="['fab', 'linkedin-in']"
+              /></a>
+              <a href="https://github.com/Samuele-Benato"
+                ><font-awesome-icon :icon="['fab', 'github']"
+              /></a>
+              <a href="https://samuele-benato.github.io/portfolio/#/"
+                ><font-awesome-icon :icon="['fas', 'user-tie']"
+              /></a>
+            </div>
+          </div>
         </div>
-        <div class="row one">
-            <div class="card">
-                <img class="samuele-white" src="/img/team/samuele-white.jpg" alt="Samuele Benato">
-                <img class="samuele-black" src="/img/team/samuele.png" alt="Samuele Benato">
-                <div class="info">
-                    <h2>Samuele Benato</h2>
-                    <p>Mi definisco un ragazzo caparbio ed intraprendente, motivarmi e motivare è il mio punto di forza.</p>
-                    <div class="social-links">  
-                        <a href="https://www.linkedin.com/in/samuele-benato-a9615a29b/"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
-                        <a href="https://github.com/Samuele-Benato"><font-awesome-icon :icon="['fab', 'github']" /></a>
-                        <a href="https://samuele-benato.github.io/portfolio/#/"><font-awesome-icon :icon="['fas', 'user-tie']" /></a>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-3 text-center desktop">
-                <span class="about-title">Team 5 classe #103</span>
-            </div>
-
-            <div class="card">
-                <img src="/img/team/pasquale.webp" alt="Pasquale Cicinelli">
-                <div class="info">
-                    <h2>Pasquale Cicinelli</h2>
-                    <p>Sono un ragazzo che persiste con forza, ma mai si arrende: la determinazione è la chiave del mio successo.</p>
-                    <div class="social-links">  
-                        <a href="https://www.linkedin.com/in/pasquale-cicinelli-64885b174/"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
-                        <a href="https://github.com/pasqualecicinelli"><font-awesome-icon :icon="['fab', 'github']" /></a>
-                    </div>
-                </div>
-            </div>
+        <div class="col-3 text-center desktop">
+          <span class="about-title">Team 5 classe #103</span>
         </div>
 
-        <div class="row two">
-            <div class="card">
-                <img src="/img/team/filippo.webp" alt="Filippo Tartaglia">
-                <div class="info">
-                    <h2>Filippo Tartaglia</h2>
-                    <p>sono un ragazzo semplice ma testardo, voglio sempre arrivare all'obbiettivo che mi sono prefissato</p>
-                    <div class="social-links">  
-                        <a href="https://www.linkedin.com/in/filippo-tartaglia-4b8382284/"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
-                        <a href="https://github.com/TartagliaFilippo"><font-awesome-icon :icon="['fab', 'github']" /></a>
-                    </div>
-                </div>
+        <div class="card">
+          <img src="/img/team/pasquale.webp" alt="Pasquale Cicinelli" />
+          <div class="info">
+            <h2>Pasquale Cicinelli</h2>
+            <p>
+              Sono un ragazzo che persiste con forza, ma mai si arrende: la
+              determinazione è la chiave del mio successo.
+            </p>
+            <div class="social-links">
+              <a
+                href="https://www.linkedin.com/in/pasquale-cicinelli-64885b174/"
+                ><font-awesome-icon :icon="['fab', 'linkedin-in']"
+              /></a>
+              <a href="https://github.com/pasqualecicinelli"
+                ><font-awesome-icon :icon="['fab', 'github']"
+              /></a>
             </div>
-
-            <div class="card">
-                <img src="/img/team/alessandro.webp" alt="Alessandro Angelelli">
-                <div class="info">
-                    <h2>Alessandro Angelelli</h2>
-                    <p>Pronto a contribuire attivamente e con successo a nuove sfide professionali.</p>
-                    <div class="social-links">  
-                        <a href="https://www.linkedin.com/in/alessandro-angelelli-9514b827b/"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
-                        <a href="https://github.com/Alessandroang"><font-awesome-icon :icon="['fab', 'github']" /></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card">
-                <img src="/img/team/vincenzo.webp" alt="Vincenzo Marcello">
-                <div class="info">
-                    <h2>Vincenzo Marcello</h2>
-                    <p>Internet ha cambiato il mondo, nel mio piccolo voglio contribuire a questo cambiamento !</p>
-                    <div class="social-links">  
-                        <a href="https://www.linkedin.com/in/vincenzo-marcello-1552972a0/"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
-                        <a href="https://github.com/VincenzoMarcello"><font-awesome-icon :icon="['fab', 'github']" /></a>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+
+      <div class="row two">
+        <div class="card">
+          <img src="/img/team/filippo.webp" alt="Filippo Tartaglia" />
+          <div class="info">
+            <h2>Filippo Tartaglia</h2>
+            <p>
+              sono un ragazzo semplice ma testardo, voglio sempre arrivare
+              all'obbiettivo che mi sono prefissato
+            </p>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/filippo-tartaglia-4b8382284/"
+                ><font-awesome-icon :icon="['fab', 'linkedin-in']"
+              /></a>
+              <a href="https://github.com/TartagliaFilippo"
+                ><font-awesome-icon :icon="['fab', 'github']"
+              /></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="/img/team/alessandro.webp" alt="Alessandro Angelelli" />
+          <div class="info">
+            <h2>Alessandro Angelelli</h2>
+            <p>
+              Pronto a contribuire attivamente e con successo a nuove sfide
+              professionali.
+            </p>
+            <div class="social-links">
+              <a
+                href="https://www.linkedin.com/in/alessandro-angelelli-9514b827b/"
+                ><font-awesome-icon :icon="['fab', 'linkedin-in']"
+              /></a>
+              <a href="https://github.com/Alessandroang"
+                ><font-awesome-icon :icon="['fab', 'github']"
+              /></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="/img/team/vincenzo.webp" alt="Vincenzo Marcello" />
+          <div class="info">
+            <h2>Vincenzo Marcello</h2>
+            <p>
+              Internet ha cambiato il mondo, nel mio piccolo voglio contribuire
+              a questo cambiamento !
+            </p>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/vincenzo-marcello-1552972a0/"
+                ><font-awesome-icon :icon="['fab', 'linkedin-in']"
+              /></a>
+              <a href="https://github.com/VincenzoMarcello"
+                ><font-awesome-icon :icon="['fab', 'github']"
+              /></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .about-background {
-  background-image: url("img/about-background.jpg");
+  background-image: url("../../../public/img/about-background.jpg");
   background-size: 100%;
   background-size: cover;
   background-position: start;
@@ -99,21 +146,28 @@
   padding-right: 1.5rem;
 }
 
-.about-title{
-    font-size: 2rem;
-    padding: 0.5rem;
-    background: linear-gradient(to bottom, rgba(34, 34, 34, 0) 0%, rgba(34, 34, 34, 0.6) 20%, rgba(34, 34, 34, 0.9) 50%, rgba(34, 34, 34, 0.6) 80%, rgba(34, 34, 34, 0) 100%);
-    color: #d7d3d3;
-    font-weight: 700;
+.about-title {
+  font-size: 2rem;
+  padding: 0.5rem;
+  background: linear-gradient(
+    to bottom,
+    rgba(34, 34, 34, 0) 0%,
+    rgba(34, 34, 34, 0.6) 20%,
+    rgba(34, 34, 34, 0.9) 50%,
+    rgba(34, 34, 34, 0.6) 80%,
+    rgba(34, 34, 34, 0) 100%
+  );
+  color: #d7d3d3;
+  font-weight: 700;
 }
-.row{
-    align-items: center;
+.row {
+  align-items: center;
 }
-.one{
-    justify-content: center;
+.one {
+  justify-content: center;
 }
-.two{
-    justify-content: space-around;
+.two {
+  justify-content: space-around;
 }
 .card {
   width: 300px;
@@ -171,21 +225,21 @@ img {
   opacity: 1;
   transform: translateY(0px);
 }
-.social-links a{
-	display: inline-block;
-	height: 40px;
-	width: 40px;
-	background-color: rgba(255,255,255,0.2);
-	margin:0 10px 10px 0;
-	text-align: center;
-	line-height: 40px;
-	border-radius: 50%;
-	color: #ffffff;
-	transition: all 0.5s ease;
+.social-links a {
+  display: inline-block;
+  height: 40px;
+  width: 40px;
+  background-color: rgba(255, 255, 255, 0.2);
+  margin: 0 10px 10px 0;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 50%;
+  color: #ffffff;
+  transition: all 0.5s ease;
 }
-.social-links a:hover{
-	color: #24262b;
-	background-color: #ffffff;
+.social-links a:hover {
+  color: #24262b;
+  background-color: #ffffff;
 }
 p {
   letter-spacing: 1px;
@@ -194,33 +248,32 @@ p {
   font-weight: 500;
 }
 
-.mobile{
-    display: none;
+.mobile {
+  display: none;
 }
 
-.samuele-white{
-    display: none;
+.samuele-white {
+  display: none;
 }
 
 @media only screen and (max-width: 992px) {
-.mobile{
+  .mobile {
     display: block;
-}
-.desktop{
+  }
+  .desktop {
     display: none;
-}
-.one{
+  }
+  .one {
     justify-content: space-around;
-}
+  }
 }
 
 @media (max-width: 939px) {
-    .samuele-white{
+  .samuele-white {
     display: block;
-}
-.samuele-black{
+  }
+  .samuele-black {
     display: none;
+  }
 }
-}
-
 </style>
