e3a6ffcae042c781f43072dee47a7c1d10a60165
Alessandroang <alessandroang94@gmail.com>
2024-01-20 16:04:39 +0100

    Add image

--- src/components/homepage/JumbotronHomepage.vue
+++ src/components/homepage/JumbotronHomepage.vue
@@ -1,41 +1,45 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-    <div class="jumbotron">
-        <div class="banner">
-            <div class="title-jumbo">
-                <h1 class="text-center fw-bold">
-                Di tutto e di più ! Cosa aspetti ? Ordina da Deliveboo !
-                </h1>
-            </div>
-        </div>     
+  <div class="jumbotron">
+    <div class="banner">
+      <div class="title-jumbo">
+        <h1 class="text-center fw-bold">
+          Di tutto e di più ! Cosa aspetti ? Ordina da Deliveboo !
+        </h1>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.banner{
-    background-color: rgba($color: #000, $alpha: 0.5) !important;
-    min-height: 30vh;
-    height: 100%;
-    margin-top: 80px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+.banner {
+  background-color: rgba($color: #000, $alpha: 0.5) !important;
+  min-height: 30vh;
+  height: 100%;
+  margin-top: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.jumbotron{
-    background-image: url("img/jumbotron.jpg");
-    background-size: cover;
-    background-position: center;
-    min-height: 30vh;
-    height: 100%;
-    margin-top: 80px;
-     
+.jumbotron {
+  background-image: url("../../../public/img/jumbotron.jpg");
+  background-size: cover;
+  background-position: center;
+  min-height: 30vh;
+  height: 100%;
+  margin-top: 80px;
 }
 
 .title-jumbo {
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.6) 20%, rgba(0, 0, 0, 0.9) 50%, rgba(0, 0, 0, 0.6) 80%, rgba(0, 0, 0, 0) 100%);
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.6) 20%,
+    rgba(0, 0, 0, 0.9) 50%,
+    rgba(0, 0, 0, 0.6) 80%,
+    rgba(0, 0, 0, 0) 100%
+  );
   padding: 0.5rem;
   width: 100%;
   color: #d7d3d3;
